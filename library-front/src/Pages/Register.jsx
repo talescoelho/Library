f5747985855c802f7984a549524d8ec2fde7e4cf
rodrigoruan <rodrigopython16@gmail.com>
2021-10-19 13:32:04 -0300

    Added mobile size

--- library-front/src/Pages/Register.jsx
+++ library-front/src/Pages/Register.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import '../css/Register.css';
+
 function Register() {
   return (
-    <div>
-      <h1>REGISTER</h1>
+    <div className="register-container">
+      <h1 className="register-title">REGISTER</h1>
+
+      <div className="register-form-1">
+        <input className="register-input" placeholder="Name" />
+        <input className="register-input" placeholder="Last Name" />
+      </div>
+
+      <input className="register-input-1" placeholder="Email" />
+      <input className="register-input-1" placeholder="CPF" />
+      <input className="register-input-1" placeholder="Birth Year" />
     </div>
   );
 }

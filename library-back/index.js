874b6b336ade405923a522cdcff79c99f3bc46a3
Jean Esteves <jean.esteves2001@gmail.com>
2021-10-19 13:37:42 -0300

    testando heroku buildpack

--- library-back/index.js
+++ library-back/index.js
@@ -8,6 +8,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Hello World!!'));
+app.get('/', (req, res) => res.send('Hello World!!!'));
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));

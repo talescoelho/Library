6b6b5e72133dde27c616bddc512597ebc5fd32ce
Jean Esteves <jean.esteves2001@gmail.com>
2021-10-19 13:47:37 -0300

    index na pasta principal

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());

a149bfa8b4db53eac42fc56c39a41e8f2b8d0db2
filizzola <juliofilizzola@hotmail.com>
2021-10-20 10:11:15 -0300

    criando rotas no arquivo user.js (router)

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/');
+router.get('/:id');
+router.post('/');
+router.put('/:id');
+router.delete('/:id');
+
+
 module.exports = router;

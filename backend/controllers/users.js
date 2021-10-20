888a99b5e85dfc78eec5c6b0a10d8dc03f4cfc72
filizzola <juliofilizzola@hotmail.com>
2021-10-20 18:54:09 -0300

    estruturando getAllUsers

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,9 +1,12 @@
-const User = require('../models/users');
+const Users = require('../models/users');
 
-const getAllUsers = async (req, res) => {};
+const getAllUsers = async (req, res) => {
+  Users.find()
+  .then((user) => res.json(user))
+  .catch((err) => console.log(err));
+};
 
 const getByUsersId = async (req, res) => {};
-
 
 module.exports = {
   getAllUsers,

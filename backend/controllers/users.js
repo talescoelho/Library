517ce18f097fdbb5272e37e824d43891fda39913
filizzola <juliofilizzola@hotmail.com>
2021-10-20 18:54:26 -0300

    estruturando getByUsersId

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -6,7 +6,17 @@ const getAllUsers = async (req, res) => {
   .catch((err) => console.log(err));
 };
 
-const getByUsersId = async (req, res) => {};
+const getByUsersId = async (req, res) => {
+  const { id } = req.params;
+  Users.findOne(
+    { _id: id },
+    { _id: true, name: true, lastName: true },
+    (err, users) => {
+      if (err) return res.status(500).json(err);
+      res.status(200).json(users);
+    }
+  );
+};
 
 module.exports = {
   getAllUsers,

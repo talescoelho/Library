4a80addb264dc28f50a1739887995d95eda4ea6f
filizzola <juliofilizzola@hotmail.com>
2021-10-20 10:38:34 -0300

    criando campos de requerimento de dados

--- backend/models/users.js
+++ backend/models/users.js
@@ -2,7 +2,28 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const usersSchema = new Schema(
-  {}
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    cpf: {
+      type: String,
+      required: true,
+    },
+    birth: {
+      type: Date,
+      required: true,
+    },
+  }
 );
 
 module.exports = mongoose.model();

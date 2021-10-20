7ec6987ba818aaa304a36c7d73ff821bf9cd55e3
filizzola <juliofilizzola@hotmail.com>
2021-10-20 18:25:07 -0300

    começando função getByUsersId

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,6 +2,10 @@ const User = require('../models/users');
 
 const getAllUsers = async (req, res) => {};
 
+const getByUsersId = async (req, res) => {};
+
+
 module.exports = {
   getAllUsers,
+  getByUsersId,
 };
